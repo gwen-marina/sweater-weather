0637dd63c310919165a8dcf1739b44ba102d6eb6
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-25 15:44:39 -0700

    Test/can return a list of hourly weather stats for the next 8 hours

--- spec/requests/api/v1/forecasts_request_spec.rb
+++ spec/requests/api/v1/forecasts_request_spec.rb
@@ -100,4 +100,43 @@ RSpec.describe 'Forecast API' do
     expect(daily[:attributes][:daily_weather][0]).to_not have_key(:moonset)
     expect(daily[:attributes][:daily_weather][0]).to_not have_key(:moon_phase)
   end
+
+  it 'can return a list of hourly weather stats for the next 8 hours', :vcr do 
+    location = 'vail,az'
+    get "/api/v1/forecast?location=#{location}"
+  
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.status).to_not eq(400)
+    
+    hourly_weather_response = JSON.parse(response.body, symbolize_names: true)
+    hourly = hourly_weather_response[:data]
+
+    expect(hourly).to have_key(:id)
+    expect(hourly[:id]).to be(nil)
+    expect(hourly[:type]).to eq('forecast')
+
+    expect(hourly[:attributes][:hourly_weather][0]).to have_key(:time)
+    expect(hourly[:attributes][:hourly_weather][0][:time]).to be_a String
+
+    expect(hourly[:attributes][:hourly_weather][0]).to have_key(:temperature)
+    expect(hourly[:attributes][:hourly_weather][0][:temperature]).to be_a Float
+
+    expect(hourly[:attributes][:hourly_weather][0]).to have_key(:conditions)
+    expect(hourly[:attributes][:hourly_weather][0][:conditions]).to be_a String
+
+    expect(hourly[:attributes][:hourly_weather][0]).to have_key(:icon)
+    expect(hourly[:attributes][:hourly_weather][0][:icon]).to be_a String
+
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:pressure)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:dew_point)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:humidity)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:wind_speed)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:wind_deg)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:wind_gust)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:feels_like)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:uvi)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:clouds)
+    expect(hourly[:attributes][:hourly_weather][0]).to_not have_key(:visibility)
+  end
 end
