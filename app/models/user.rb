e7e094eb2d22e1382d0142cdd6b6b9c6ba913369
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-25 17:26:01 -0700

    Feat/added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates_uniqueness_of :email
+  validates_presence_of :email
+  validates_uniqueness_of :api_key
+  validates_presence_of :api_key
+
+  has_secure_password
 end
