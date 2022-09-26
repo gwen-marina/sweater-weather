5b3fdd9646caded0e351fef283960ead7e5ffb5a
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-26 09:22:45 -0700

    Feat/initialize and attr_reader for book

---
+++ app/poros/book.rb
@@ -0,0 +1,12 @@
+class Book
+  attr_reader :title,
+              :publisher,
+              :isbn
+
+  def initialize(data)
+    @title = data[:title]
+    @publisher = data[:publisher]
+    @isbn = data[:isbn]
+  end
+end
+
