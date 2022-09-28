4fe4172d845b2a0a035f4b82fb377de3349ffd70
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-27 21:15:50 -0700

    Tried to use a different method for api key generation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,14 +7,11 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password_digest }
     it { should have_secure_password }
- 
   end
-
-  describe 'Instance Methods' do 
-    it 'can generate a random api key associated with a user when a user is created' do 
-       smudger = User.create!(email: 'imadog@dogmail.com', password: 'steak', password_confirmation:'steak')
-       smudger.generate_api_key
-        expect(smudger.generate_api_key).to be_a(String)
-    end
+ 
+  it 'can generate a random api key associated with a user when a user is created' do 
+      smudger = User.create!(email: 'imadog@dogmail.com', password: 'steak', password_confirmation:'steak')
+      smudger.generate_api_key
+      expect(smudger.generate_api_key).to be_a(String)
   end
 end
