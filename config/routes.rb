c9e2688969c1fb1a2decb86466a4d0e30bb9f4d1
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-27 21:07:20 -0700

    Route for roadtrip api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  get '/api/v1/forecast', to: 'api/v1/forecast#weather_forecast'
- 
  post '/api/v1/users', to: 'api/v1/users#create'
-
  post '/api/v1/sessions', to: 'api/v1/sessions#create'
+ post '/api/v1/road_trip', to: 'api/v1/road_trips#create'
 end
