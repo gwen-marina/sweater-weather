495d0770dbf3761c1bd1970168fca5c9b96260bf
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-27 21:16:26 -0700

    Fix/updated render status method since it was returning incorrect error

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::SessionsController < ApplicationController
    
   def create
     user = User.find_by(email: params[:email]) 
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: UserSerializer.create_user(user)
     else
-      render json: { status: 400, message: "Email or Password is incorrect, please try again." }
+      render status: 400, json: { message: "Email or Password is incorrect, please try again." }
     end
   end
 end
